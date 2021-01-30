2d8e12d75dbd81db2b2a96b73c1fb40b0d9bac42
Millie Lin <linxinmin@gmail.com>
2021-01-30 17:28:13 +0800

    Added text

--- public/bundle.js
+++ public/bundle.js
@@ -2648,27 +2648,47 @@ const sizeScale = ordinal()
     .domain(['apple', 'lemon'])
     .range(['50', '30']);
 
+const xPosition = (d, i) => i * 120 + 60;
+
 const fruitBowl = (selection, props) => {
     const {fruits, height} = props;
+
     const circles = selection.selectAll('circle')
         .data(fruits, d => d.id);
-
     // Enter & Update
     circles
         .enter().append('circle')
-        .attr('cx', 0)
+        .attr('cx', xPosition)
         .attr('cy', height / 2)
         .attr('r', 0)
         .merge(circles)
         .attr('fill', d => colorScale(d.type))
         .transition().duration(1000)
-            .attr('cx', (d, i) => i * 120 + 60)
-            .attr('r', d => sizeScale(d.type));
-
+        .attr('cx', xPosition)
+        .attr('r', d => sizeScale(d.type));
     // exit & Update
     circles.exit()
         .transition().duration(1000)
-            .attr('r', 0)
+        .attr('r', 0)
+        .remove();
+
+    const text = selection.selectAll('text')
+        .data(fruits);
+    text
+        .enter().append('text')
+        .attr('x', (d, i) => i * 120 + 60)
+        .attr('y', height / 2 + 120)
+        .attr('opacity', 0)
+        .merge(text)
+        .attr('opacity', 1)
+        .text(d => d.type)
+        .transition().duration(1000)
+        .attr('opacity', 1)
+        .attr('x', (d, i) => i * 120 + 60);
+
+    text.exit()
+        .transition().duration(1000)
+        .attr('opacity', 0)
         .remove();
 };
 
